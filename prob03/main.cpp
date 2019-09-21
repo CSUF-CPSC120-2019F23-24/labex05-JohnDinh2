040574133ffc59f977d776a861d19f6c7a198192
johndinh391 <johndinh391@cs104u-1d.acad.fullerton.edu>
2019-09-21 13:19:29 -0700

    Rectangle made.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
 // Random Rectangle
+#include <iostream>
+#include <ctime>
+#include <string>
+int main()
+{
+  unsigned seed = time(0);
+  srand(seed);
+  const int length = 40;
+  int width = rand() % length + 1;
+  std::string rectangle_line;
+  rectangle_line.assign(width, '#');
+  std::cout << rectangle_line << '\n';
+  std::cout << rectangle_line << std::endl;
+  std::cout << rectangle_line << std::endl;
+}
